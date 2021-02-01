20984e87f8047829aab97490b0d848b3b3f3efd4
RNolan19 <rnolan19@gmail.com>
2021-02-01 13:59:01 -0500

    setting up Netflix ap infrastructure

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron'
+
+export default function Home() {
+  return (
+    <div className="">
+        <Jumbotron.Container>
+            <Jumbotron.Title>Hello</Jumbotron.Title>
+            <Jumbotron.SubTitle>Hello</Jumbotron.SubTitle>
+        </Jumbotron.Container>
+    </div>
+  )
+}
